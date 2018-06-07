249657a2ee9038fcf9aec992e9a74a40cdfb20bf
Joshua Majait <entertainment.ripple@gmail.com>
2018-06-07 14:36:14 +0800

    updated

--- visualization.js
+++ visualization.js
@@ -3,7 +3,6 @@
 function Point(x, y) {
     this.x = x;
     this.y = y;
-    
 }
 function drawLine(a, b){
     context.beginPath();
@@ -23,44 +22,27 @@ function drawCircle(a,rad){
     context.stroke();
 }
 function displayImage(lnk,a,b){
-    console.log("drawing image on", a, b);
+
     this.img = new Image();
     this.img.src = lnk;
     console.log(img);
     context.drawImage(img, a, b, 603, 800);
 }
-document.getElementById('canvas').addEventListener('click',function(evt){
-   console.log("ScalpArr.push(new Point(" + (evt.clientX-444) + ', ' + (evt.clientY-10) + "));");
-    },false);
+
 //-----------------------------------------runTime------------------------------------------------------
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 //Background
 context.fillStyle = "#ffffff";
 context.fillRect(0, 0, canvas.width, canvas.height);//clear screen
-// Rule of Thirds
-// context.strokeStyle = "#ff0000";
-// var ROT1 = new Point(canvas.width/3,0);
-// var ROT2 = new Point(canvas.width/3,canvas.height);
-// drawLine(ROT1, ROT2);
-// var ROT3 = new Point(canvas.width*2/3,0);
-// var ROT4 = new Point(canvas.width*2/3,canvas.height);
-// drawLine(ROT3, ROT4);
-// var ROT5 = new Point(0,canvas.height/3);
-// var ROT6 = new Point(canvas.width,canvas.height/3);
-// drawLine(ROT5, ROT6);
-// var ROT7 = new Point(0,canvas.height*2/3);
-// var ROT8 = new Point(canvas.width,canvas.height*2/3);
-// drawLine(ROT7, ROT8);
-// // //Marquardt's Phi Mask
-// var img = document.getElementById("Mask");
-//Redraw
+
 context.strokeStyle = "#00ff00";
 drawCircle(new Point(318,318), 8);
 drawCircle(new Point(318,318), 21);
 drawCircle(new Point(canvas.width-318,318), 8);
 drawCircle(new Point(canvas.width-318,318), 21);
-//face
+
+//face mask
 var goldenRatioPointsArr = new Array();
 goldenRatioPointsArr.push(new Point((canvas.width/2),500));
 goldenRatioPointsArr.push(new Point((413),489));
@@ -76,7 +58,7 @@ goldenRatioPointsArr.push(new Point(428, 445));
 goldenRatioPointsArr.push(new Point(424, 330));
 goldenRatioPointsArr.push(new Point(389, 281));
 goldenRatioPointsArr.push(new Point(256, 236));
-goldenRatioPointsArr.push(new Point(196, 277));//cheek
+goldenRatioPointsArr.push(new Point(196, 277));
 goldenRatioPointsArr.push(new Point(256, 261));
 goldenRatioPointsArr.push(new Point(375, 296));
 goldenRatioPointsArr.push(new Point(410, 349));
@@ -101,7 +83,7 @@ goldenRatioPointsArr.push(new Point(355, 659));
 goldenRatioPointsArr.push(new Point(327, 620));
 goldenRatioPointsArr.push(new Point(354, 597));
 goldenRatioPointsArr.push(new Point(173, 347));
-goldenRatioPointsArr.push(new Point(196, 277));//cheek
+goldenRatioPointsArr.push(new Point(196, 277));
 goldenRatioPointsArr.push(new Point(268, 326));
 goldenRatioPointsArr.push(new Point(282, 306));
 goldenRatioPointsArr.push(new Point(317, 295));
@@ -123,30 +105,15 @@ goldenRatioPointsArr.push(new Point(214, 466));
 goldenRatioPointsArr.push(new Point(298, 578));
 goldenRatioPointsArr.push(new Point(381, 452));
 
-// context.beginPath();
-// context.moveTo(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
-// for(var i = 1; i < goldenRatioPointsArr.length; i++){
-//     console.log(goldenRatioPointsArr[i].x, goldenRatioPointsArr[i].y);
-//     context.lineTo(goldenRatioPointsArr[i].x, goldenRatioPointsArr[i].y);
-// }
-// context.stroke();
-
-// context.beginPath();
-// context.moveTo(canvas.width - goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
-// for(var i = 1; i < goldenRatioPointsArr.length; i++){
-//     context.lineTo(canvas.width - goldenRatioPointsArr[i].x, goldenRatioPointsArr[i].y);
-// }
-// context.stroke();
-
 context.strokeStyle = "#0000ff";
-//hair
+
 var ScalpArr = new Array();
-ScalpArr.push(new Point(196, 277));//cheek
+ScalpArr.push(new Point(196, 277));
 ScalpArr.push(new Point(196, 118));
 ScalpArr.push(new Point(336, 13));
 ScalpArr.push(new Point(canvas.width - 336, 13));
 ScalpArr.push(new Point(canvas.width - 196, 118));
-ScalpArr.push(new Point(canvas.width - 196, 277));//cheek
+ScalpArr.push(new Point(canvas.width - 196, 277));
 ScalpArr.push(new Point(canvas.width - 173, 347));
 ScalpArr.push(new Point(canvas.width - 225, 598));
 ScalpArr.push(new Point(canvas.width - 291, 678));
@@ -155,118 +122,106 @@ ScalpArr.push(new Point(379, 744));
 ScalpArr.push(new Point(291, 678));
 ScalpArr.push(new Point(225, 598));
 ScalpArr.push(new Point(173, 347));
-ScalpArr.push(new Point(196, 277));//cheek
-
-// context.beginPath();
-// context.moveTo(ScalpArr[0].x, ScalpArr[0].y);
-// for(var i = 1; i < ScalpArr.length; i++){
-//     context.lineTo(ScalpArr[i].x, ScalpArr[i].y);
-// }
-// context.stroke();
-
+ScalpArr.push(new Point(196, 277));
 
 //---------------------------AUDIO------------------------
 var audio = new Audio();
-audio.src = 'audio/track3.mp3';
+audio.src = 'audio/increasingHz.mp3';
 audio.controls = true;
 audio.loop = false;
 audio.autoplay = false;
+var A = new Point(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
+var B = new Point(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
+var ctr = 0;
+var mode = 0;
 
-window.addEventListener("load", initMp3Player, false);//plays the audio
+window.addEventListener("load", initMp3Player, false);
 
 function initMp3Player(){
 	document.getElementById('audio_box').appendChild(audio);
-	aContext = new AudioContext(); // AudioContext object instance
-	// analyser = new AnalyserNode();
-	analyser = aContext.createAnalyser(); // AnalyserNode method
+	aContext = new AudioContext();
+	analyser = aContext.createAnalyser();
 	splitter = aContext.createChannelSplitter();
 	canvas = document.getElementById('analyser_render');
 	ctx = canvas.getContext('2d');
-	// Re-route audio playback into the processing graph of the AudioContext
 	source = aContext.createMediaElementSource(audio);
 	source.connect(analyser);
     analyser.connect(aContext.destination);
     
-    
 	frameLooper();
 }
-// frameLooper() animates any style of graphics you wish to the audio frequency
-// Looping at the default frame rate that the browser provides(approx. 60 FPS)
 
 //---------------------------VISUALIZATION------------------------------
 function frameLooper(){
-    console.log("Playing!");
 	window.requestAnimationFrame(frameLooper);
 	fbc_array = new Uint8Array(analyser.frequencyBinCount);
 	analyser.getByteFrequencyData(fbc_array);
 	bars = 100;
 
 
-    var newX, newY, freqColor;
-    
-    freqColor = (fbc_array[0] / 2);
-    if(freqColor < 50)
-        context.strokeStyle = '#666'; // Color of the bars
+    var newX, newY, frequency, amplitude;
+    frequency = 0;
+    amplitude = 0;
+
+    for(var i = 0; i < fbc_array.length ; i++){
+        if(fbc_array[i] > amplitude){
+            amplitude = fbc_array[i] / 2;
+            frequency = (i * 360)/1024;
+        }
+    }
+
+    if(amplitude > 0){
+
+    if(amplitude%3 == 0)
+        context.strokeStyle = '#666'; // Draw Color
     else{
-        context.strokeStyle = '#fff';
+        context.strokeStyle = '#fff'; // Erase Color
     }
-    //face
-    context.beginPath();
-    context.moveTo(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
-    for(var i = 1; i < goldenRatioPointsArr.length ; i++){
-        freqColor = (fbc_array[0] / 2);
-        newX = (freqColor*(Math.cos(freqColor))) - (freqColor*(Math.sin(freqColor))) + goldenRatioPointsArr[i].x;
-        newY = (freqColor*(Math.sin(freqColor))) + (freqColor*(Math.cos(freqColor))) + goldenRatioPointsArr[i].y;
-        
-        context.lineTo(newX, newY);
-
-    }
-    context.stroke();
 
     context.beginPath();
-    context.moveTo(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
-    for(var i = 1; i < goldenRatioPointsArr.length; i++){
-        freqColor = (fbc_array[0] / 2);
-        newX = (freqColor*(Math.cos(freqColor))) - (freqColor*(Math.sin(freqColor))) + goldenRatioPointsArr[i].x;
-        newY = (freqColor*(Math.sin(freqColor))) + (freqColor*(Math.cos(freqColor))) + goldenRatioPointsArr[i].y;
-        
-        context.lineTo(canvas.width - newX + canvas.width*2, newY);
+    context.moveTo(A.x, A.y);
+    switch(mode){
+        case 0:
+            context.moveTo(A.x, A.y);
+            if(ctr < goldenRatioPointsArr.length){
+                B = new Point(goldenRatioPointsArr[ctr].x, goldenRatioPointsArr[ctr].y);
+                newX = (amplitude*(Math.cos(frequency))) - (amplitude*(Math.sin(frequency))) + B.x;
+                newY = (amplitude*(Math.sin(frequency))) + (amplitude*(Math.cos(frequency))) + B.y;
+                
+                context.lineTo(newX, newY);
+                context.stroke();
 
+                context.beginPath();
+                context.moveTo(canvas.width*3 - A.x, A.y);
+                context.lineTo(canvas.width*3 - newX, newY);
+                context.stroke();
+                A = new Point(newX, newY);
+            }
+            else{
+                mode = 1;
+                ctr = -1;
+                A = new Point(newX, newY);
+            }
+            break;
+        case 1:
+            if(ctr < ScalpArr.length){
+                B = new Point( ScalpArr[ctr].x, ScalpArr[ctr].y);
+                newX = (amplitude*(Math.cos(frequency))) - (amplitude*(Math.sin(frequency))) + B.x;
+                newY = (amplitude*(Math.sin(frequency))) + (amplitude*(Math.cos(frequency))) + B.y;
+                
+                context.lineTo(newX, newY);
+                context.stroke();
+                A = new Point(newX, newY);
+            }
+            else{
+                mode = 0;
+                ctr = -1;
+                A = new Point(goldenRatioPointsArr[0].x, goldenRatioPointsArr[0].y);
+            }
+            break;
+        default:
     }
     context.stroke();
-    //background
-    context.beginPath();
-    for(var i = 0; i < ScalpArr.length ; i++){
-        freqColor = (fbc_array[1] / 2);
-        newX = (freqColor*(Math.cos(freqColor))) - (freqColor*(Math.sin(freqColor))) + ScalpArr[i].x;
-        newY = (freqColor*(Math.sin(freqColor))) + (freqColor*(Math.cos(freqColor))) + ScalpArr[i].y;
-        
-        context.lineTo(newX, newY);
-
+    ctr++;
     }
-    context.stroke();
-
-	// for (var i = 0; i < bars; i++) {
-	// 	bar_x = i * 3;
-	// 	bar_width = 2;
-	// 	bar_height = -(fbc_array[i] / 2);
-	// 	// fillRect( x, y, width, height ) // Explanation of the parameters below
-	// 	ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-
-	// }
-}
-//---------------------------CONVERTION-----------------------------
-function rePosition(x,y,loudness,frequency){
-    //frequency is position in the color wheel
-    //loudness is distance from original position
-    //loudness
-    var freqColor = 36 * frequency;//0. minimum, 360, maximum
-    var newX = (x*(Math.cos(freqColor))) - (y*(Math.sin(freqColor)));
-    var newY = (x*(Math.sin(freqColor))) + (y*(Math.cos(freqColor)));
-    
-    context.lineTo(goldenRatioPointsArr[i].x, goldenRatioPointsArr[i].y);
-}
-
-function ifInside(x,y){
-    
 }
